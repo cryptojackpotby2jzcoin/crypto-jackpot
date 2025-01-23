2b4542175b95ec93c95dc3342927efc4b136e31d
cryptojackpotby2jzcoin <cafflions@gmail.com>
2025-01-23 00:19:17 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = 'YOUR_TELEGRAM_BOT_TOKEN';
+const token = '8090884490:AAFe6j6fjvLzi8XPIsP2TrP1JYwHG3MVpyA';
 const bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/\/spin/, async (msg) => {
